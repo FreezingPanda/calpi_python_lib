2f4d750a143b90743bd891271232b00c2ca7b360
FreezingPanda <pickledchimeraarmy@gmail.com>
2024-11-07 22:17:13 +1000

    Hid Email, Changed Dev alias in setup.py

--- calpi/setup.py
+++ calpi/setup.py
@@ -5,8 +5,8 @@ setup(
     name="calpi",
     version="0.1",
     packages=find_packages(),
-    author="DevMaster42",  # Your alias here
-    author_email="your.email@example.com",  # Or leave it blank if you prefer
+    author="PandaDev.py",  # Your alias here
+    author_email="None :)",  # Or leave it blank if you prefer
     description="Calculate Pi easily with the Calpi library",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
