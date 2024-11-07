fa2a106e317d87bbe4a62348262996fb6589f99d
FreezingPanda <pickledchimeraarmy@gmail.com>
2024-11-07 22:20:13 +1000

    Prepared for release

--- calpi/setup.py
+++ calpi/setup.py
@@ -10,7 +10,7 @@ setup(
     description="Calculate Pi easily with the Calpi library",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/mylibrary",  
+    url="https://github.com/FreezingPanda/calpi_python_lib",  
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
